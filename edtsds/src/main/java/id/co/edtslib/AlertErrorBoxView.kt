62c094ca3595f86545c48a609ec4a8abc55855eb
tonikustiana <toni.kustiana@sg-dsa.com>
2022-09-16 16:40:29 +0700

    alert error box

--- edtsds/src/main/java/id/co/edtslib/AlertErrorBoxView.kt
+++ edtsds/src/main/java/id/co/edtslib/AlertErrorBoxView.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.util.AttributeSet
 import androidx.core.content.ContextCompat
 
-class AlertWarningBoxView: AlertSuccessBoxView {
+class AlertErrorBoxView: AlertSuccessBoxView {
     constructor(context: Context) : super(context)
     constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
@@ -14,9 +14,9 @@ class AlertWarningBoxView: AlertSuccessBoxView {
     )
 
     init {
-        binding.clContent.setBackgroundResource(R.drawable.bg_support_warning)
-        binding.textView.setTextColor(ContextCompat.getColor(context, R.color.colorSupportWarning))
-        binding.imageView.setImageResource(R.drawable.ic_alert_warning)
+        binding.clContent.setBackgroundResource(R.drawable.bg_support_error)
+        binding.imageView.setImageResource(R.drawable.ic_alert_error)
+        binding.textView.setTextColor(ContextCompat.getColor(context, R.color.colorSupportError))
 
     }
 
