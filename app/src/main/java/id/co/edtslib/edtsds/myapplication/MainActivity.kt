7be8300793fe9d32163c42b0b7e6a11dade315b6
tonikustiana <toni.kustiana@sg-dsa.com>
2022-09-20 10:28:42 +0700

    textfield

--- app/src/main/java/id/co/edtslib/edtsds/myapplication/MainActivity.kt
+++ app/src/main/java/id/co/edtslib/edtsds/myapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package id.co.edtslib.edtsds.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatDelegate
+import id.co.edtslib.edtsds.textfield.TextFieldView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +12,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val tvPin = findViewById<TextFieldView>(R.id.tvPin)
+        tvPin.error = "error woi"
     }
 }
