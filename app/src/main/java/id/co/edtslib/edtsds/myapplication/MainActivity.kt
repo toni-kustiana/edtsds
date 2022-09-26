0221dbded1152d42092bf8be4d1261dae616ab0c
tonikustiana <toni.kustiana@sg-dsa.com>
2022-09-26 07:18:55 +0700

    otp remaining view

--- app/src/main/java/id/co/edtslib/edtsds/myapplication/MainActivity.kt
+++ app/src/main/java/id/co/edtslib/edtsds/myapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package id.co.edtslib.edtsds.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatDelegate
+import id.co.edtslib.edtsds.otpremainingview.OtpRemainingView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +12,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val exampleView = findViewById<OtpRemainingView>(R.id.exampleView)
+        exampleView.interval = 60
+        exampleView.start()
 
 
     }
