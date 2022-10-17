45dd1ddcfb78187eb1e415dff4197d917a90902d
tonikustiana <toni.kustiana@sg-dsa.com>
2022-10-17 12:55:27 +0700

    support multiple dialog

--- edtsds/src/main/java/id/co/edtslib/edtsds/bottom/BottomLayoutDialog.kt
+++ edtsds/src/main/java/id/co/edtslib/edtsds/bottom/BottomLayoutDialog.kt
@@ -11,12 +11,13 @@ import id.co.edtslib.edtsds.databinding.DialogBottomLayoutBinding
 open class BottomLayoutDialog(context: Context):
     Dialog(context,R.style.BottomLayoutDialog) {
     companion object {
-        lateinit var dialog: BottomLayoutDialog
+        private lateinit var dialog: BottomLayoutDialog
+
         fun showSwipeTray(context: Context, title: String, contentView: View,
                           tray: Boolean = true, cancelable: Boolean = false,
                           titleView: View? = null, titleDivider: Boolean = true,
-                          popup: Boolean = false) {
-            dialog = BottomLayoutDialog(context)
+                          popup: Boolean = false): BottomLayoutDialog {
+            val dialog = BottomLayoutDialog(context)
             dialog.binding.bottomLayout.title = title
             dialog.binding.bottomLayout.tray = tray
             dialog.binding.bottomLayout.cancelable = cancelable
@@ -33,24 +34,18 @@ open class BottomLayoutDialog(context: Context):
             dialog.setCancelable(cancelable)
             dialog.setCanceledOnTouchOutside(false)
             dialog.show()
+
+            BottomLayoutDialog.dialog = dialog
+
+            return dialog
         }
 
         fun showTray(context: Context, title: String, contentView: View, titleView: View? = null,
-                     titleDivider: Boolean = true, popup: Boolean = false) {
+                     titleDivider: Boolean = true, popup: Boolean = false) =
             showSwipeTray(context, title, contentView, tray = false, cancelable = true,
                 titleView = titleView, titleDivider = titleDivider, popup = popup)
-        }
 
         fun close() {
-            try {
-                dialog.dismiss()
-            }
-            catch (ignore: Exception) {
-
-            }
-        }
-
-        fun close(dialog: Dialog) {
             try {
                 dialog.dismiss()
             }
@@ -61,6 +56,15 @@ open class BottomLayoutDialog(context: Context):
     }
 
     val binding = DialogBottomLayoutBinding.inflate(layoutInflater)
+
+    fun close() {
+        try {
+            dismiss()
+        }
+        catch (ignore: Exception) {
+
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
