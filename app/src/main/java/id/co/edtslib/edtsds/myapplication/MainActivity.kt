7e360c38cbe5ae733f46cf9d9cd452abef93396a
tonikustiana <toni.kustiana@sg-dsa.com>
2022-12-06 16:40:45 +0700

    change popup

--- app/src/main/java/id/co/edtslib/edtsds/myapplication/MainActivity.kt
+++ app/src/main/java/id/co/edtslib/edtsds/myapplication/MainActivity.kt
@@ -1,9 +1,12 @@
 package id.co.edtslib.edtsds.myapplication
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import id.co.edtslib.edtsds.list.skeleton.RecyclerShimmerView
+import id.co.edtslib.edtsds.popup.Popup
+import id.co.edtslib.edtsds.popup.PopupDelegate
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,11 +15,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val menuSkeletonView = findViewById<RecyclerShimmerView>(R.id.menuSkeletonView)
-        menuSkeletonView.show()
-        menuSkeletonView.postDelayed({
-            menuSkeletonView.hide()
-        }, 5000)
+       Popup.show(activity = this, title = "Test", message = "ajajja", negativeButton = "Neg",
+        positiveButton = "Pos", negativeClickListener = object : PopupDelegate {
+               override fun onClick(popup: Popup, view: View) {
+                   popup.dismiss()
+               }
+           }, positiveClickListener = object : PopupDelegate {
+               override fun onClick(popup: Popup, view: View) {
+               }
+           })
 
     }
 }
--- edtsds/src/main/java/id/co/edtslib/edtsds/popup/Popup.kt
+++ edtsds/src/main/java/id/co/edtslib/edtsds/popup/Popup.kt
@@ -7,7 +7,6 @@ import android.content.res.Resources
 import android.os.Bundle
 import android.view.Gravity
 import android.view.View
-import android.view.View.OnClickListener
 import android.view.Window
 import android.view.WindowManager
 import androidx.appcompat.widget.LinearLayoutCompat
@@ -19,7 +18,7 @@ import id.co.edtslib.edtsds.R
 import id.co.edtslib.edtsds.databinding.DialogPopupBinding
 
 class Popup private constructor(context: Context, private val view: View?, themeResId: Int) : Dialog(context, themeResId) {
-    var dismissible = false
+    private var dismissible = false
 
     enum class Orientation {
         Horizontal, Vertical
@@ -27,135 +26,128 @@ class Popup private constructor(context: Context, private val view: View?, theme
 
     @SuppressLint("StaticFieldLeak")
     companion object {
-        private var popup: Popup? = null
-
         fun show(activity: FragmentActivity, title: String?, message: String,
-                 positiveButton: String?, positiveClickListener: OnClickListener?,
+                 positiveButton: String?, positiveClickListener: PopupDelegate?,
                  gravity: Int = Gravity.START, themeResId: Int = 0, dismissible: Boolean = false,
-                 isHtml: Boolean = false) {
+                 isHtml: Boolean = false) =
             show(activity, title, message, positiveButton, null,
                 positiveClickListener, null, themeResId =
                 themeResId, gravity = gravity, dismissible = dismissible, isHtml = isHtml)
-        }
 
         fun show(activity: FragmentActivity, title: String?, message: String,
                  positiveButton: String?, negativeButton: String?,
-                 positiveClickListener: OnClickListener?, negativeClickListener: OnClickListener?,
+                 positiveClickListener: PopupDelegate?, negativeClickListener: PopupDelegate?,
                  orientation: Orientation = Orientation.Horizontal, gravity: Int = Gravity.START,
-                 themeResId: Int = 0, dismissible: Boolean = false, isHtml: Boolean = false) {
+                 themeResId: Int = 0, dismissible: Boolean = false, isHtml: Boolean = false): Popup {
 
-            if (popup == null) {
-                popup = Popup(activity, null, themeResId)
+            val popup = Popup(activity, null, themeResId)
 
-                popup?.dismissible = dismissible
+            popup.dismissible = dismissible
 
-                popup?.binding?.tvTitle?.isVisible = title?.isNotEmpty() == true
-                popup?.binding?.tvTitle?.text = if (isHtml && title != null) HtmlCompat.fromHtml(title,
-                    HtmlCompat.FROM_HTML_MODE_LEGACY) else title
-                popup?.binding?.tvTitle?.gravity = gravity
+            popup.binding.tvTitle.isVisible = title?.isNotEmpty() == true
+            popup.binding.tvTitle.text = if (isHtml && title != null) HtmlCompat.fromHtml(title,
+                HtmlCompat.FROM_HTML_MODE_LEGACY) else title
+            popup.binding.tvTitle.gravity = gravity
 
-                popup?.binding?.tvMessage?.text = if (isHtml) HtmlCompat.fromHtml(message,
-                    HtmlCompat.FROM_HTML_MODE_LEGACY) else message
-                popup?.binding?.tvMessage?.gravity = gravity
+            popup.binding.tvMessage.text = if (isHtml) HtmlCompat.fromHtml(message,
+                HtmlCompat.FROM_HTML_MODE_LEGACY) else message
+            popup.binding.tvMessage.gravity = gravity
 
-                popup?.binding?.bvNegative?.isVisible = negativeButton != null
-                popup?.binding?.bvNegative?.text = negativeButton
+            popup.binding.bvNegative.isVisible = negativeButton != null
+            popup.binding.bvNegative.text = negativeButton
 
-                val negativeClick = negativeClickListener
-                    ?: OnClickListener {
-                        popup?.dismiss()
+
+            popup.binding.bvNegative.setOnClickListener {
+                if (negativeClickListener == null) {
+                    popup.dismiss()
+                }
+                else {
+                    negativeClickListener.onClick(popup, it)
+                }
+            }
+
+            popup.binding.bvPositive.text = positiveButton
+            popup.binding.bvPositive.setOnClickListener {
+                if (positiveClickListener == null) {
+                    popup.dismiss()
+                }
+                else {
+                    positiveClickListener.onClick(popup, it)
+                }
+            }
+
+
+            if (orientation == Orientation.Vertical) {
+                popup.binding.llButton.orientation = LinearLayoutCompat.VERTICAL
+
+                val lp1 = popup.binding.bvNegative.layoutParams as LinearLayoutCompat.LayoutParams?
+                lp1?.width = LinearLayoutCompat.LayoutParams.MATCH_PARENT
+                lp1?.marginEnd = 0
+
+                val lp2 = popup.binding.bvPositive.layoutParams as LinearLayoutCompat.LayoutParams?
+                lp2?.width = LinearLayoutCompat.LayoutParams.MATCH_PARENT
+                lp2?.marginStart = 0
+                lp2?.topMargin = activity.resources.getDimensionPixelSize(R.dimen.dimen_8dp)
+
+                // tukar posisi negative dengan positive
+
+                popup.binding.bvNegative.variant = ButtonView.ButtonVariant.Primary
+                popup.binding.bvNegative.text = positiveButton
+                popup.binding.bvNegative.setOnClickListener {
+                    if (positiveClickListener == null) {
+                        popup.dismiss()
                     }
-                popup?.binding?.bvNegative?.setOnClickListener(negativeClick)
-
-
-                popup?.binding?.bvPositive?.text = positiveButton
-                val positiveClick = positiveClickListener
-                    ?: OnClickListener {
-                        popup?.dismiss()
+                    else {
+                        positiveClickListener.onClick(popup, it)
                     }
-                popup?.binding?.bvPositive?.setOnClickListener(positiveClick)
-
-
-                if (orientation == Orientation.Vertical) {
-                    popup?.binding?.llButton?.orientation = LinearLayoutCompat.VERTICAL
-
-                    val lp1 = popup?.binding?.bvNegative?.layoutParams as LinearLayoutCompat.LayoutParams?
-                    lp1?.width = LinearLayoutCompat.LayoutParams.MATCH_PARENT
-                    lp1?.marginEnd = 0
-
-                    val lp2 = popup?.binding?.bvPositive?.layoutParams as LinearLayoutCompat.LayoutParams?
-                    lp2?.width = LinearLayoutCompat.LayoutParams.MATCH_PARENT
-                    lp2?.marginStart = 0
-                    lp2?.topMargin = activity.resources.getDimensionPixelSize(R.dimen.dimen_8dp)
-
-                    // tukar posisi negative dengan positive
-
-                    popup?.binding?.bvNegative?.variant = ButtonView.ButtonVariant.Primary
-                    popup?.binding?.bvNegative?.text = positiveButton
-                    popup?.binding?.bvNegative?.setOnClickListener(positiveClick)
-
-                    popup?.binding?.bvPositive?.variant = ButtonView.ButtonVariant.Outline
-                    popup?.binding?.bvPositive?.text = negativeButton
-                    popup?.binding?.bvPositive?.setOnClickListener(negativeClick)
-
                 }
 
-                popup?.show()
+                popup.binding.bvPositive.variant = ButtonView.ButtonVariant.Outline
+                popup.binding.bvPositive.text = negativeButton
+                popup.binding.bvPositive.setOnClickListener {
+                    if (negativeClickListener == null) {
+                        popup.dismiss()
+                    }
+                    else {
+                        negativeClickListener.onClick(popup, it)
+                    }
+                }
 
-                val width = Resources.getSystem().displayMetrics.widthPixels * 0.9f
-                popup?.window?.setLayout(
-                    width.toInt(),
-                    WindowManager.LayoutParams.WRAP_CONTENT
-                )
             }
+
+            popup.show()
+
+            val width = Resources.getSystem().displayMetrics.widthPixels * 0.9f
+            popup.window?.setLayout(
+                width.toInt(),
+                WindowManager.LayoutParams.WRAP_CONTENT
+            )
+
+            return popup
         }
 
-        fun showFullScreen(view: View, themeResId: Int = 0, dismissible: Boolean = false) {
+        fun showFullScreen(view: View, themeResId: Int = 0, dismissible: Boolean = false) =
             show(view, width = WindowManager.LayoutParams.MATCH_PARENT.toFloat(),
                 height = WindowManager.LayoutParams.MATCH_PARENT, themeResId = themeResId,
                 dismissible = dismissible)
-        }
 
         fun show(view: View, width: Float = 0.9f,
             height: Int = WindowManager.LayoutParams.WRAP_CONTENT,
                  themeResId: Int = 0, dismissible: Boolean = false) {
-            popup = Popup(view.context, view, themeResId)
-            popup?.dismissible = dismissible
-            popup?.show()
+            val popup = Popup(view.context, view, themeResId)
+            popup.dismissible = dismissible
+            popup.show()
 
-            val w = if (width == WindowManager.LayoutParams.WRAP_CONTENT.toFloat())
-                WindowManager.LayoutParams.WRAP_CONTENT else
-                if (width == WindowManager.LayoutParams.MATCH_PARENT.toFloat())
-                    WindowManager.LayoutParams.MATCH_PARENT else
-                    (Resources.getSystem().displayMetrics.widthPixels * 0.9f).toInt()
+            val w = when (width) {
+                WindowManager.LayoutParams.WRAP_CONTENT.toFloat() -> WindowManager.LayoutParams.WRAP_CONTENT
+                WindowManager.LayoutParams.MATCH_PARENT.toFloat() -> WindowManager.LayoutParams.MATCH_PARENT
+                else -> (Resources.getSystem().displayMetrics.widthPixels * 0.9f).toInt()
+            }
 
-            popup?.window?.setLayout(
+            popup.window?.setLayout(
                 w,
                 height
             )
-        }
-
-        fun close() {
-            try {
-                popup?.dismiss()
-                popup = null
-            }
-            catch (e: Exception) {
-                // ignore it
-            }
-        }
-
-        fun close(action: () -> Unit) {
-            try {
-                popup?.setOnDismissListener {
-                    action()
-                }
-                popup?.dismiss()
-                popup = null
-            }
-            catch (e: Exception) {
-                // ignore it
-            }
         }
     }
 
@@ -170,13 +162,5 @@ class Popup private constructor(context: Context, private val view: View?, theme
         window?.decorView?.setBackgroundResource(android.R.color.transparent)
 
         setCancelable(dismissible)
-
-        setOnDismissListener {
-            popup = null
-        }
-
-        setOnCancelListener {
-            popup = null
-        }
     }
 }
