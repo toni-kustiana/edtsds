4dde39c6d4f9a9265bb9763cc9b0fa2d160f823f
tonikustiana <toni.kustiana@sg-dsa.com>
2022-12-13 14:01:28 +0700

    textfield popup

--- app/src/main/java/id/co/edtslib/edtsds/myapplication/MainActivity.kt
+++ app/src/main/java/id/co/edtslib/edtsds/myapplication/MainActivity.kt
@@ -1,14 +1,8 @@
 package id.co.edtslib.edtsds.myapplication
 
 import android.os.Bundle
-import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
-import id.co.edtslib.edtsds.list.skeleton.RecyclerShimmerView
-import id.co.edtslib.edtsds.popup.Popup
-import id.co.edtslib.edtsds.popup.PopupDelegate
-import id.co.edtslib.edtsds.textfield.TextFieldDelegate
 import id.co.edtslib.edtsds.textfield.TextFieldView
 
 class MainActivity : AppCompatActivity() {
@@ -19,11 +13,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val aaa = findViewById<TextFieldView>(R.id.aaa)
-        aaa.delegate = object : TextFieldDelegate {
-            override fun onChanged(input: String?) {
-                Toast.makeText(this@MainActivity, "asdsad", Toast.LENGTH_SHORT).show()
-            }
-        }
+        aaa.text = "TextFieldDelegate TextFieldDelegate TextFieldDelegate TextFieldDelegate"
+        //aaa.text = "TextFieldDelegate"
+
+        val bbb = findViewById<TextFieldView>(R.id.bbb)
+        bbb.text = "Test"
+        bbb.editText?.hint = "Abah Adilah"
 
     }
 }
