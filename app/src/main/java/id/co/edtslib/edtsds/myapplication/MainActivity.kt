42ba805ec95ef61d32260286b5d507b3cf4698b7
tonikustiana <toni.kustiana@sg-dsa.com>
2023-02-17 18:00:51 +0700

    fix number

--- app/src/main/java/id/co/edtslib/edtsds/myapplication/MainActivity.kt
+++ app/src/main/java/id/co/edtslib/edtsds/myapplication/MainActivity.kt
@@ -1,8 +1,11 @@
 package id.co.edtslib.edtsds.myapplication
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import id.co.edtslib.edtsds.textfield.TextFieldDelegate
+import id.co.edtslib.edtsds.textfield.TextFieldView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +14,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val aa = findViewById<TextFieldView>(R.id.etName)
+        aa.delegate = object : TextFieldDelegate {
+            override fun onChanged(input: String?) {
+                Toast.makeText(this@MainActivity, input, Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
