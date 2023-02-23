190cde686eb41d770f13016416f3e366615afc44
tonikustiana <toni.kustiana@sg-dsa.com>
2023-02-23 08:45:43 +0700

    optview enable/disabled

--- app/src/main/java/id/co/edtslib/edtsds/myapplication/MainActivity.kt
+++ app/src/main/java/id/co/edtslib/edtsds/myapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package id.co.edtslib.edtsds.myapplication
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import id.co.edtslib.edtsds.otpview.OtpView
 import id.co.edtslib.edtsds.textfield.TextFieldView
 
 class MainActivity : AppCompatActivity() {
@@ -12,8 +13,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val textFieldView = findViewById<TextFieldView>(R.id.textFieldView)
-        textFieldView.isActivated = true
+        val otpView = findViewById<OtpView>(R.id.otpView)
+        otpView.isEnabled = true
 
     }
 }
