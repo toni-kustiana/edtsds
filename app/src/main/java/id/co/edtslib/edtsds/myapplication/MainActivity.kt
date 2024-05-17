02feff2d8bc369cbde00549b80144397c2afcea9
tonikustiana <toni.kustiana@sg-dsa.com>
2024-05-17 10:06:44 +0700

    boarding view: navigation vertical alignment

--- app/src/main/java/id/co/edtslib/edtsds/myapplication/MainActivity.kt
+++ app/src/main/java/id/co/edtslib/edtsds/myapplication/MainActivity.kt
@@ -5,7 +5,11 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import id.co.edtslib.edtsds.ButtonView
+import id.co.edtslib.edtsds.boarding.BoardingData
+import id.co.edtslib.edtsds.boarding.BoardingView
 import id.co.edtslib.edtsds.chips.sliding.ChipItemData
 import id.co.edtslib.edtsds.chips.sliding.SlidingChipsView
 import id.co.edtslib.edtsds.popup.Popup
@@ -21,49 +25,13 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_main)
 
-        val slidingStepperView = findViewById<SlidingStepperView>(R.id.slidingStepperView)
+        val json = "[{\"title\": \"Belanja Mudah\", \"description\": \"One stop online store yang menyediakan berbagai macam produk dalam satu aplikasi\", \"image\": \"ic_onboarding_1\"}, {\"title\": \"Beragam Varian Produk\", \"description\": \"Menyediakan ribuan pilihan produk yang lengkap dengan harga terbaik untuk segala kebutuhan\", \"image\": \"ic_onboarding_2\"}, {\"title\": \"Banyak Promonya\", \"description\": \"Nikmati beragam promo menarik dari Klik Indomaret untuk kamu\", \"image\": \"ic_onboarding_3\"}]"
 
-        val adapter = slidingStepperView.adapter as StepperAdapter
-        for (i in 0..100) {
-            adapter.list.add(1)
-        }
-
-        adapter.notifyDataSetChanged()
-
-        val slidingChipView = findViewById<SlidingChipsView<ChipItemData>>(R.id.slidingChipView)
-        slidingChipView.items = mutableListOf(
-            ChipItemData("Testing", "https://cdn-klik-dev.edts.id/assets/mobile/ico-top/Tickets.png", ),
-            ChipItemData("Testing", "https://cdn-klik-dev.edts.id/assets/mobile/ico-top/Tickets.png", "https://cdn-klik-dev.edts.id/assets/mobile/ico-top/Tickets-sel.png"),
-            ChipItemData("Testing", R.drawable.ic_grocery),
-            ChipItemData("Testing", R.drawable.ic_grocery, R.drawable.ic_grocery_selected),
+        val boardingView = findViewById<BoardingView>(R.id.boardingView)
+        boardingView.list = Gson().fromJson<List<BoardingData>?>(
+            json,
+            object : TypeToken<List<BoardingData>?>() {}.type
         )
-
-        val btnAsal = findViewById<ButtonView>(R.id.btnAsal)
-
-        btnAsal.setOnClickListener {
-            Popup.show(
-                activity = this,
-                title = "Hapus 2 Barang?",
-                message = "Barang yang kamu pilih akan dihapus dari keranjang.",
-                positiveButton = "Hapus",
-                negativeButton = "Pindahkan ke Favorit",
-                positiveClickListener = object : PopupDelegate {
-                    override fun onClick(popup: Popup, view: View) {
-                        popup.dismiss()
-
-                    }
-                },
-                negativeClickListener = object : PopupDelegate {
-                    override fun onClick(popup: Popup, view: View) {
-                        popup.dismiss()
-
-                    }
-                },
-                orientation = Popup.Orientation.Vertical,
-                dismissible = true,
-                negativeButtonStyle = ButtonView.ButtonVariant.Alternative
-            )
-        }
     }
 
 }
