adc937b64b5d7da8ad138445dc40c8873dcd6c5a
Muhammad Ramdhan <muhammad.ramdhan@sg-edts.com>
2025-11-03 11:32:27 +0700

    FEAT: Add onCollapse delegate at tray X (cancel) button and onDismiss()

--- edtsds/src/main/java/id/co/edtslib/edtsds/bottom/BottomLayout.kt
+++ edtsds/src/main/java/id/co/edtslib/edtsds/bottom/BottomLayout.kt
@@ -130,6 +130,7 @@ class BottomLayout: FrameLayout {
             binding.ivCancel.isVisible = cancelable
             binding.ivCancel.setOnClickListener {
                 isVisible = false
+                delegate?.onCollapse()
             }
 
             binding.flTitle.isVisible = title?.isNotEmpty() == true || cancelable || ! popup
--- edtsds/src/main/java/id/co/edtslib/edtsds/bottom/BottomLayoutDialog.kt
+++ edtsds/src/main/java/id/co/edtslib/edtsds/bottom/BottomLayoutDialog.kt
@@ -36,6 +36,7 @@ open class BottomLayoutDialog(context: Context, themeResId: Int): Dialog(context
             dialog.binding.bottomLayout.delegate = object : BottomLayoutDelegate {
                 override fun onDismiss() {
                     dialog.dismiss()
+                    delegate?.onDismiss()
                 }
 
                 override fun onCollapse() {
